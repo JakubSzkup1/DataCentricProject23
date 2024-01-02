09442f130ed7468cf7f2fc5fc0c4df413b800ec8
JakubSzkup1 <jakub281@hotmail.com>
2024-01-02 01:38:57 +0000

    worked on fixing manager validation

--- mongodbDAO.js
+++ mongodbDAO.js
@@ -29,27 +29,26 @@ var addManager = function (managerId, name, salary) {
     return new Promise((resolve, reject) => {
 
 
-        // //Heres is the validation for manager id, name and salary //struggled with making validation work
-        //  //manager id must be 4 chars
-        //  if (managerId.length !== 4) {
-        //     reject(new Error('Manager ID must be 4 characters long.')); //error message returned
-        //     return;
-        // }
-        // //name must be > 5 chars
-        // if (name.length <= 5) {
-        //     reject(new Error('Name must be more than 5 characters long.')); //error message returned
-        //     return;
-        // }
+         //Heres is the validation for manager id, name and salary //struggled with making all validation work - now when manager is added its not added to the form
+          //manager id must be 4 chars
+          if (managerId.length !== 4) {
+             reject(new Error('Manager ID must be 4 characters long.')); //error message returned
+             return;
+         }
+         //name must be > 5 chars
+         if (name.length <= 5) {
+             reject(new Error('Name must be more than 5 characters long.')); //error message returned
+             return;
+         }
 
-        // //salary must be between 30k and 70k
-        // salary = Number(salary);//coverting the string to a number
-        // if(salary <3000 || salary > 70000){
-        //     reject(new Error('Salary must be between 30,000 and 70,000.')); //error message
-        //     return;
-        // }   
+         //salary must be between 30k and 70k
+         salary = Number(salary);//coverting the string to a number
+         if(salary <3000 || salary > 70000){
+             reject(new Error('Salary must be between 30,000 and 70,000.')); //error message
+             return;
+         }   
 
     
-
         coll.insertOne({
             _id: managerId,
             name: name,
